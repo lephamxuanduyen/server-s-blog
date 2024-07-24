2d180679517a6e9cd7c48cf18d2c0b26316b4a3f
nau <lephamxuanduyen@gmail.com>
2024-07-24 07:37:09 +0700

    remove public folder and update vercel.json

--- api/server.js
+++ api/server.js
@@ -1,22 +1,31 @@
+// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
-const path = require('path')
 
 const server = jsonServer.create()
+
+// Uncomment to allow write operations
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('db.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
+// const router = jsonServer.router(db)
+
+// Comment out to allow write operations
 const router = jsonServer.router('db.json')
+
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-
-// Cấu hình rewriter nếu cần thiết
+// Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
-
-// Sử dụng router cho server
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
 })
 
+// Export the Server API
 module.exports = server
