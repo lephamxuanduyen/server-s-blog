1e37f6cf02cd7a401cbd016a8c1efa39c27afdb9
nau <lephamxuanduyen@gmail.com>
2024-07-26 15:49:52 +0700

    improve: upload header_image

---
+++ publish/shuffle/index.ts
@@ -0,0 +1,85 @@
+import hang2_1 from "./hang2_1.jpg";
+import hang2_2 from "./hang2_2.jpg";
+import hang2_3 from "./hang2_3.jpg";
+import hang2_4 from "./hang2_4.jpg";
+import hang2_5 from "./hang2_5.jpg";
+import hang2_6 from "./hang2_6.jpg";
+import hang2_7 from "./hang2_7.jpg";
+import hang2_8 from "./hang2_8.jpg";
+import hang2_9 from "./hang2_9.jpg";
+import hang2_10 from "./hang2_10.jpg";
+import hang2_11 from "./hang2_11.jpg";
+import hang2_12 from "./hang2_12.jpg";
+import hang2_13 from "./hang2_13.jpg";
+import hang2_14 from "./hang2_14.jpg";
+import hang2_15 from "./hang2_15.jpg";
+import hang2_16 from "./hang2_16.jpg";
+
+const SHUFFLE = [
+  {
+    id: 1,
+    src: hang2_1,
+  },
+  {
+    id: 2,
+    src: hang2_2,
+  },
+  {
+    id: 3,
+    src: hang2_3,
+  },
+  {
+    id: 4,
+    src: hang2_4,
+  },
+  {
+    id: 5,
+    src: hang2_5,
+  },
+  {
+    id: 6,
+    src: hang2_6,
+  },
+  {
+    id: 7,
+    src: hang2_7,
+  },
+  {
+    id: 8,
+    src: hang2_8,
+  },
+  {
+    id: 9,
+    src: hang2_9,
+  },
+  {
+    id: 10,
+    src: hang2_10,
+  },
+  {
+    id: 11,
+    src: hang2_11,
+  },
+  {
+    id: 12,
+    src: hang2_12,
+  },
+  {
+    id: 13,
+    src: hang2_13,
+  },
+  {
+    id: 14,
+    src: hang2_14,
+  },
+  {
+    id: 15,
+    src: hang2_15,
+  },
+  {
+    id: 16,
+    src: hang2_16,
+  },
+];
+
+export default SHUFFLE;
